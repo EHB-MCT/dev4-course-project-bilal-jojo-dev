39e9426dfa34ed712bf9808420c08c44db330fdb
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-04-11 00:34:07 +0200

    initial commit

---
+++ loans/src/main/kotlin/ehb/multimedia/loans/controllers/ItemsController.kt
@@ -0,0 +1,17 @@
+package ehb.multimedia.loans.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("items")
+class ItemsController {
+
+    @GetMapping()
+    fun getItems(): String {
+        return "Test"
+    }
+
+}
