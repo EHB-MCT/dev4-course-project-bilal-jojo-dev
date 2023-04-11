0e4d52930c3069024944571c3ba9ae1e988ea379
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-04-11 16:07:19 +0200

    last test

--- loans/src/main/kotlin/ehb/multimedia/loans/controllers/ItemsController.kt
+++ loans/src/main/kotlin/ehb/multimedia/loans/controllers/ItemsController.kt
@@ -16,7 +16,7 @@ class ItemsController {
 
     @GetMapping("item")
     fun getItem(): String {
-        return "This is on the bilal branch";
+        return "This is on the Jojo branch";
     }
 
 }
