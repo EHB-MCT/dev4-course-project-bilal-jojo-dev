daf6b7746f757b951f1d832196a0881012880819
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-04-11 10:41:27 +0200

    map structure

---
+++ loans/src/main/kotlin/ehb/multimedia/loans/controllers/UsersController.kt
@@ -0,0 +1,16 @@
+package ehb.multimedia.loans.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("users")
+class UsersController {
+
+    @GetMapping("")
+    fun getUsers(): String {
+        return "This is the users class"
+    }
+
+}
