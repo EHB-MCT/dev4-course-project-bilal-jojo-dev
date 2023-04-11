9b975c902b6b7e59aed1e138de11c9b7b263cf3a
bilalfwz <bilal.fawaz@student.ehb.be>
2023-04-11 21:28:21 +0200

    making models

---
+++ loans/src/main/kotlin/ehb/multimedia/loans/models/Reminder.kt
@@ -0,0 +1,7 @@
+package ehb.multimedia.loans.models
+
+class Reminder {
+    var id: Double = 0.0
+    var loanerId: Double = 0.0
+    var type: String = ""
+}
---
+++ loans/src/main/kotlin/ehb/multimedia/loans/models/copyOfItem.kt
@@ -0,0 +1,8 @@
+package ehb.multimedia.loans.models
+
+class copyOfItem {
+    var serial: Double = 0.0
+    var status: String = ""
+    var lastLoaner: String = ""
+    var remarks: String = ""
+}
