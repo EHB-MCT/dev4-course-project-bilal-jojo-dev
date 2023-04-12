82c9840f29c8505ab21ca2d8c5cabc6168c59544
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-04-12 19:56:05 +0200

    push repo

--- loans/src/main/kotlin/ehb/multimedia/loans/repositories/userRepository.kt
+++ loans/src/main/kotlin/ehb/multimedia/loans/repositories/userRepository.kt
@@ -1,5 +1,6 @@
 package ehb.multimedia.loans.repositories
 
+import ehb.multimedia.loans.models.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
