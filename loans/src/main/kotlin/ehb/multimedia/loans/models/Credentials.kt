48b31aa9e6b01aab1aa0fd1b46211d44be4b7384
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-04-24 17:56:27 +0200

    hotfix: fixed credentials to database

---
+++ loans/src/main/kotlin/ehb/multimedia/loans/models/Credentials.kt
@@ -0,0 +1,10 @@
+package ehb.multimedia.loans.models
+
+class Credentials {
+
+    val databaseUser: String = "DEV4030";
+    val databasePassword: String = "86472935";
+    val databaseName: String = "DEV4030"
+
+
+}
