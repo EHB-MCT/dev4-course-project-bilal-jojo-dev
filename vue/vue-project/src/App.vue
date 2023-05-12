bc7af9e7bbf30e5a7af88da1fff76aaf7afc788e
bilalfwz <bilal.fawaz@student.ehb.be>
2023-05-12 17:42:32 +0200

    Login function first work on home page before opening items en users

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,25 +1,48 @@
 
-
-
-
 <template>
   <div id="app">
     <nav>
       <ul>
-        <li>
+        <li v-if="isLoggedIn">
           <router-link to="/items">Items</router-link>
         </li>
-        <li>
+        <li v-if="isLoggedIn">
           <router-link to="/users">Users</router-link>
         </li>
-        <li>
+        <li v-if="!isLoggedIn">
           <router-link to="/login">Login</router-link>
+        </li>
+        <li v-if="isLoggedIn" @click="logout">
+          Logout
         </li>
       </ul>
     </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoggedIn: !!localStorage.getItem('authToken'),
+    }
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('authToken');
+      this.isLoggedIn = false;
+      this.$router.push('/login');
+    },
+  },
+  created() {
+    this.$router.beforeEach((to, from, next) => {
+      this.isLoggedIn = !!localStorage.getItem('authToken');
+      next();
+    });
+  },
+}
+</script>
 
 <style scoped>
 nav {
