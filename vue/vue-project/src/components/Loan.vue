c19f9b7410d0d8e5332cc47cfd53bd1e94fd759a
bilalfwz <bilal.fawaz@student.ehb.be>
2023-05-13 17:53:05 +0200

    Add styling to the loan page

--- vue/vue-project/src/components/Loan.vue
+++ vue/vue-project/src/components/Loan.vue
@@ -1,32 +1,49 @@
 <template>
     <div class="loan">
-      <h2>Loan</h2>
-      <form @submit.prevent="reserve">
-        <div>
-          <label for="startDate">Start Date:</label>
-          <input type="date" id="startDate" v-model="startDate">
-        </div>
-        <button type="submit">Reserve</button>
-        <p v-if="errorMessage">{{ errorMessage }}</p>
-      </form>
+        <h2>Loan</h2>
+        <form @submit.prevent="reserve">
+            <div>
+                <label for="startDate">Start Date:</label>
+                <input type="date" id="startDate" v-model="startDate">
+            </div>
+            <button type="submit">Reserve</button>
+            <p v-if="errorMessage">{{ errorMessage }}</p>
+        </form>
     </div>
-  </template>
+</template>
   
-  <script>
-  export default {
+<script>
+export default {
     data() {
-      return {
-        startDate: '',
-        errorMessage: ''
-      }
+        return {
+            startDate: '',
+            errorMessage: ''
+        }
     },
     methods: {
-      reserve() {
-        console.log('Reserving from start date:', this.startDate);
-      }
+        reserve() {
+            console.log('Reserving from start date:', this.startDate);
+        }
     }
-  }
-  </script>
-  
+}
+</script>
+<style scoped>
+.loan {
+    width: 300px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    background-color: #f9f9f9;
+}
+
+.loan input {
+    width: 100%;
+    margin: 5px 0;
+}
+
+.loan button {
+    margin-top: 10px;
+}
+</style>
   
   
